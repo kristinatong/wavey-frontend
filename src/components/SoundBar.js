ca7fdbc0181bcd79c39f23dcf4b47649d3075cd2
kristinatong <kristina.tong@gmail.com>
2018-12-03 14:32:56 -0500

    updated sound bar

--- src/components/SoundBar.js
+++ src/components/SoundBar.js
@@ -3,15 +3,92 @@ import { connect } from 'react-redux';
 import * as actions from '../actions/index';
 import { API_ENDPOINT } from '../adapters/index'
 import Input from '@material-ui/core/Input';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FilledInput from '@material-ui/core/FilledInput';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import { MuiThemeProvider } from '@material-ui/core/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { colors } from '../App'
 import { Image, List } from 'semantic-ui-react'
 import ReactPlayer from 'react-player'
+
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+  list:{
+    width: '10%',
+    height: '10%'
+  }
+  // formControl: {
+  //   minWidth: 120,
+  //   margin: 0,
+  //   fullWidth: true,
+  //   backgroundColor: '#9ee',
+  //   display: 'flex',
+  //   wrap: 'nowrap'
+  // }
+});
 
 class SoundBar extends Component{
   state = {
@@ -45,7 +122,7 @@ class SoundBar extends Component{
 
 
   handleAttach = () => {
-    if(!Object.keys(this.props.selectedSprite).length === 0){
+    if(!Object.keys(this.props.selectedSprite).length == 0){
       fetch(`${API_ENDPOINT}/api/v1/sounds/${this.state.selectedSoundBar.id}`)
         .then(r => r.json())
         .then(data => {
